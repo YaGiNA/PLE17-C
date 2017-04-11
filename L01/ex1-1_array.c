877283b40c9bafd2bce31a6405b73a17bede0bb8
YaGiNA <yahyu10101@gmail.com>
2017-04-11 15:24:25 +0900

    ex1-1::bug:Debuging ex1-2:Add func. ex1-1: Reusing same array defined by main() ex1-2: Add func. of summing of nodes wout/ sorting

--- L01/ex1-1_array.c
+++ L01/ex1-1_array.c
@@ -1,29 +1,22 @@
 #include <stdio.h>
 
-void raw_array(void){
-  int i = 0, j;
-  double nums[20];
-  double tmp, sum_raw_array = 0;
-  sum_raw_array = 0;
-  for(i=0; i<20; i++) {
-    scanf("%lf", &nums[i]);
+void raw_array(double *nums){
+  printf("----------------そのまま入力した場合の要素----------------\n");
+  double sum_raw_array;
+  int i;
+  for(i=0; i<21; ++i){
     sum_raw_array += nums[i];
   }
-
-  printf("入力順: %e\n", sum_raw_array);
+  printf("入力順: %g\n", sum_raw_array);
 }
 
-void sorted_array(void){
-  int n = 0, i = 0, j;
-  double nums[20];
-  double tmp, sum_sorted_array = 0;
-  for(i=0; i<20; i++) {
-    scanf("%lf", &nums[i]);
-  }
-
+void sorted_array(double *nums){
+  printf("----------------昇順ソートをした場合の要素----------------\n");
+  int i, j;
+  double sum_sorted_array, tmp;
   /* 数値を昇順にソート */
-  for (i=0; i<20;i++) {
-    for (j=i+1; j<20; j++) {
+  for (i=0; i<21;i++) {
+    for (j=i+1; j<21; j++) {
       if (nums[i] > nums[j]) {
         tmp =  nums[i];
         nums[i] = nums[j];
@@ -32,13 +25,23 @@ void sorted_array(void){
     }
   }
 
-  for(i=0; i<20; i++){
+  for(i=0; i<21; i++){
     sum_sorted_array += nums[i];
+    printf("%g\n", nums[i]);
   }
-  printf("ソート順: %20.18e\n", sum_sorted_array);
+  printf("ソート順: %g\n", sum_sorted_array);
 }
 
 int main(void){
-  raw_array();
-  sorted_array();
+  int i = 0, j;
+  double nums[21];
+  double tmp, sum_raw_array = 0;
+  sum_raw_array = 0;
+  for(i=0; i<21; i++) {
+    scanf("%lf", &nums[i]);
+    sum_raw_array += nums[i];
+    printf("%g\n", nums[i]);
+  }
+  raw_array(nums);
+  sorted_array(nums);
 }
